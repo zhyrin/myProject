b0c0440b400ad5bb1230e7bd6b89b4f5b5f02643
zhyrin <bjorn.ahrsjo@gmail.com>
2017-11-06 10:08:39 +0100

    New files add another html page and move files to separate folders

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// (function (){
+//
+//   var myGrades =  [95, 43, 6, 7, 45, 75];
+//
+//   var average = function(){
+//     var total = myGrades.reduce(function(accumulator, item){
+//       return accumulator + item}, 0);
+//
+//       return 'Your average grade is' + total/myGrades.length + '.';
+//
+//   }
+//
+//   var failing = function(){
+//     var failingGrades = myGrades.filter(function(item){
+//       return item < 70;});
+//
+//       return 'You failed' + failingGrades.length + 'times.';
+//   }
+//
+//   console.log(failing());
+//   console.log(global);
+// //'You failed 2 times.'
+// }());
+
+$(document).ready(function){
+
+
+});
